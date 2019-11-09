b0487169d59c6ca0521a3b2cdd985c9e07ff03ef
GC Jyothi Prasanna <43312457+JyothiPrasanna768@users.noreply.github.com>
2019-11-09 15:28:54 +0530

    Create Naive_bayes.py

---
+++ Naive_Bayes_Classifier/Naive_bayes.py
@@ -0,0 +1,37 @@
+#step:1 :- import libraries
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+#step:2 :- import dataset
+iris =load_iris()
+
+label_names = iris['target_names']
+labels = iris['target']
+feature_names = iris['feature_names']
+features = iris['data']
+
+print(label_names)
+print(labels[0])
+print(feature_names[0])
+print(features[0])
+
+#step:3 :- divide dataset into training dataset and testing dataset
+# Here I take 20% original dataset as test dataset
+train,test,train_labels,test_labels = train_test_split(features,labels,
+                                                        test_size = 0.20,
+                                                        random_state = 42)
+
+# step:4 :- Building and evaluating the model using Naive Bayes Classifier                                                      
+gnb = GaussianNB()
+model = gnb.fit(train,train_labels)
+
+preds = gnb.predict(test)
+print("Prediction")
+print(preds)
+
+#step:5 :- Evaluating models accuracy
+print("accuracy")
+print(accuracy_score(test_labels,preds))
